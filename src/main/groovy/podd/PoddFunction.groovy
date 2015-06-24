73db02e318f4655d201091ddce71e1f7ecb2b002
crosalot <crosalot#gmail.com>
2015-06-24 16:12:58 +0700

    add call django api when detect state chage

--- src/main/groovy/podd/PoddFunction.groovy
+++ src/main/groovy/podd/PoddFunction.groovy
@@ -1,8 +1,24 @@
 package podd
 
+import groovyx.net.http.HTTPBuilder
+import groovyx.net.http.ContentType
+import groovyx.net.http.Method
+
 class PoddFunction {
-	public static final int promoteState(int reportId, String state) {
-		println 'call api reportId: ' + reportId + ', state: ' + state
+	public static final int promoteState(int reportId, String responseUrlPattern) {
+		def url = responseUrlPattern.replace('{{id}}', reportId.toString())
+		println 'call api: ' + url
+
+		def http = new HTTPBuilder(url)
+
+		http.request(Method.POST) {
+			response.success = { resp ->
+		        println "Success! ${resp.status}"
+		    }
+		    response.failure = { resp ->
+		        println "Request failed with status ${resp.status}"
+		    }
+		}
 		return reportId
 	}
 }
